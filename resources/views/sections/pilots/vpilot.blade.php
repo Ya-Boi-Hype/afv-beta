1e9f7d32c3ae2a53836f40f1f4ef3f917886dd37
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-08-20 02:54:49 +0200

    New version

--- resources/views/sections/pilots/vpilot.blade.php
+++ resources/views/sections/pilots/vpilot.blade.php
@@ -14,7 +14,7 @@
           <p>To use new voice as a vPilot user is very simple! You just need to download and run the installer available at the link below.
           This installer will create a new vPilot shortcut with <i>'AFV Beta'</i> in its name.<br>
           Use this shortcut to connect to the AFV server (you may select any server in vPilot's list).</p>
-          <a class="btn btn-primary" href="http://vpilot.metacraft.com/Assets/Files/Installers/AfvBeta/vPilotAFVBeta-Setup-2.2.2.7.exe">Download vPilot for AFV</a>
+          <a class="btn btn-primary" href="http://vpilot.metacraft.com/Assets/Files/Installers/AfvBeta/vPilotAFVBeta-Setup-2.2.2.12.exe">Download vPilot for AFV</a>
         </div>
       </div>
       <!-- /.card -->
