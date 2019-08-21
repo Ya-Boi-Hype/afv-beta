2dc3b8e9f3f68e2c531415e30cf6b63c00420fc4
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-08-21 22:39:31 +0200

    Fix

--- app/Http/Controllers/admin/ApprovalController.php
+++ app/Http/Controllers/admin/ApprovalController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Approval;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\AfvApicontroller;
+use App\Http\Controllers\AfvApiController;
 
 class ApprovalController extends Controller
 {
