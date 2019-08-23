3a00df847fe64b67ef6079b8ae2888c17e6eec67
Aidan Stevens <aidan@aidanstevens.ca>
2019-08-23 20:21:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/admin/ApprovalController.php
+++ app/Http/Controllers/admin/ApprovalController.php
@@ -97,10 +97,10 @@ class ApprovalController extends Controller
         $newApprovals = Approval::available()->pending();
 
         foreach ($newApprovals->cursor() as $approval) {
-            try{
+            try {
                 $requestData = ['Username' => (string) $approval->user()->id, 'Enabled' => true];
                 AfvApiController::doPUT('users/enabled', [$requestData]);
-            } catch (\Exception $e){
+            } catch (\Exception $e) {
                 continue;
             }
             $approval->setAsApproved();
