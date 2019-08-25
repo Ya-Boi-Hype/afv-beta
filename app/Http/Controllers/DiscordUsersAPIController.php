00951666b87a170fe0b05914ec1783b69361a33b
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-08-25 12:58:41 +0200

    SUP or not

--- app/Http/Controllers/DiscordUsersAPIController.php
+++ app/Http/Controllers/DiscordUsersAPIController.php
@@ -12,8 +12,10 @@ class DiscordUsersAPIController extends Controller
         $output = [];
 
         foreach ($accounts as $account) {
+            $rating = $account->user->rating_atc;
+            $sup = ($rating == 'SUP' || $rating == 'ADM');
             $name = mb_convert_case(mb_strtolower($account->user->full_name.' - '.$account->user->id), MB_CASE_TITLE, 'UTF-8');
-            $output[$account->id] = ['display_name' => $name, 'approved' => $account->user->approved];
+            $output[$account->id] = ['display_name' => $name, 'approved' => $account->user->approved, 'sup' => $sup];
         }
 
         return response($output)->header('Content-Type', 'application/json');
