683c4b6e3a6b008e9c719fca77c1e76611a05735
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-09-04 14:33:37 +0200

    Image naming because CaSiNg

--- resources/views/sections/pilots/xpilot.blade.php
+++ resources/views/sections/pilots/xpilot.blade.php
@@ -15,7 +15,7 @@
           <p>This installer will create a new xPilot application and install a plugin to your XPlane folder. When connecting you can select any server, as it will always connect to the AFV Beta.<p>
           <p>To use xPilot, you will need to remove/disable other pilot clients like xSquawkBox, swift, etc... from your X-Plane plugins folder</p>
           <p class="text-danger"><b>Don't forget to go into the settings menu and calibrate your microphone by ensuring it is bouncing in the green bar when speaking!</b></p>
-          <div class="w-100 text-center mb-3"><img src="{{ asset('images/demos/xpilot_settings.png') }}" class="img-fluid rounded"></div>
+          <div class="w-100 text-center mb-3"><img src="{{ asset('images/demos/xpilot_settings(1).png') }}" class="img-fluid rounded"></div>
 		      <a class="btn btn-primary" href="http://xpilot.clowd.io/installers/beta/xPilot-Setup-1.0.0.6.exe">Download xPilot for AFV</a>
         </div>
 		 
