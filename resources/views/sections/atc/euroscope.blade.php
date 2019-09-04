b1125daed583639348c2bf7c413a5bc6ad5f7588
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-09-04 14:55:03 +0200

    spacing

--- resources/views/sections/atc/euroscope.blade.php
+++ resources/views/sections/atc/euroscope.blade.php
@@ -11,14 +11,12 @@
           <h1 class="m-0 text-dark">Euroscope</h1>
         </div>
         <div class="card-body">
-            <p>To use Euroscope with AFV, you will need to download and install the standalone client and the Euroscope Update with NoVVL Shortcut available below.<br>
-  <br>
-			<a class="btn btn-primary mx-1 mt-1" href="{{ route('client.download') }}">Download Standalone Client</a>
-			<a class="btn btn-primary mx-1 mt-1" href="http://afv-beta.vatsim.net/downloads/EuroScopeBeta32a21-With-AFV-Shortcut.zip">Download EuroScope Update and Shortcut</a>
-			
-  <br>
+            <p>To use Euroscope with AFV, you will need to download and install the standalone client and the Euroscope Update with NoVVL Shortcut available below.</p>
+
+			      <a class="btn btn-primary mx-1 mt-1" href="{{ route('client.download') }}">Download Standalone Client</a>
+			      <a class="btn btn-primary mx-1 mt-1" href="http://afv-beta.vatsim.net/downloads/EuroScopeBeta32a21-With-AFV-Shortcut.zip">Download EuroScope Update and Shortcut</a>
   
-			Then, open Euroscope <b> via the shortcut in the beta download.</b> <BR><BR>In the connection window, set <b>afv-beta-fsd.vatsim.net</b> as the server and connect to it.<BR><BR>
+			      <p class="mt-3">Then, open Euroscope <b> via the shortcut in the beta download.</b> <BR><BR>In the connection window, set <b>afv-beta-fsd.vatsim.net</b> as the server and connect to it.<BR><BR>
             <BR><BR><strong>It is essential that you use the shortcut provided otherwise you will interfere with the live network.</strong><BR><BR>
             </b>. Finally, run the standalone client you previously installed and log in using your VATSIM Account.</p>
             
