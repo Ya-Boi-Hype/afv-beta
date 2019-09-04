a61f1487244b703124dd9fd0fe8930f90c2ba4dd
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-09-05 00:00:08 +0200

    Remove details

--- routes/web.php
+++ routes/web.php
@@ -89,10 +89,6 @@ Route::middleware('auth')->group(function () {
     Route::middleware('admin')->group(function () {
         Route::resource('transceivers', 'TransceiverController')->only('destroy');
         Route::resource('stations', 'StationController')->only('destroy');
-        Route::view('details', function (Request $request) {
-            $approval = \App\Models\Approval::find($request->input('approval'));
-            dd($approval->user);
-        });
     });
 });
 
