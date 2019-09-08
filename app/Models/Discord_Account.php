269b5b7dafa45271f1bf9ae1b67b5def0b5c6c5f
Aidan Stevens <aidan@aidanstevens.ca>
2019-09-08 23:17:42 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Models/Discord_Account.php
+++ app/Models/Discord_Account.php
@@ -9,7 +9,7 @@ class Discord_Account extends Model
     public $timestamps = false;
 
     protected $fillable = [
-        'user_id', 'id', 'mode', 'nickname'
+        'user_id', 'id', 'mode', 'nickname',
     ];
 
     public function user()
@@ -19,8 +19,7 @@ class Discord_Account extends Model
 
     public function getNameAttribute()
     {
-        switch($this->mode)
-        {
+        switch ($this->mode) {
             case 0: return $this->mode0;
             case 1: return $this->mode1;
             case 2: return $this->mode2;
@@ -32,12 +31,12 @@ class Discord_Account extends Model
     {
         return mb_convert_case(mb_strtolower($this->user->full_name.' - '.$this->user->id), MB_CASE_TITLE, 'UTF-8');
     }
-    
+
     public function getMode1Attribute() // Name - CID
     {
         return mb_convert_case(mb_strtolower($this->user->name_first.' - '.$this->user->id), MB_CASE_TITLE, 'UTF-8');
     }
-    
+
     public function getMode2Attribute() // CID
     {
         return (string) $this->user->id;
