554c1f708b112702b33372fbc3929a5560ad97db
Aidan Stevens <aidan@aidanstevens.ca>
2019-09-14 12:17:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Console/Commands/ApproveRandom.php
+++ app/Console/Commands/ApproveRandom.php
@@ -41,7 +41,7 @@ class ApproveRandom extends Command
      */
     public function handle()
     {
-        echo 'Random approvals in progress...' . PHP_EOL;
+        echo 'Random approvals in progress...'.PHP_EOL;
         try {
             $user = User::findOrFail($this->argument('actAs'));
         } catch (ModelNotFoundException $e) {
