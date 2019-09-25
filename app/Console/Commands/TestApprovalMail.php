b6026b68bb29be85e9ebdd0779fcc664eff48358
Aidan Stevens <aidan@aidanstevens.ca>
2019-09-25 23:49:41 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Console/Commands/TestApprovalMail.php
+++ app/Console/Commands/TestApprovalMail.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\User;
+use Illuminate\Console\Command;
 use App\Notifications\HfTestApprovalNotification;
 
 class TestApprovalMail extends Command
@@ -44,6 +44,7 @@ class TestApprovalMail extends Command
             $user = User::findOrFail($cid);
         } catch (ModelNotFoundException $e) {
             echo 'User could not be found';
+
             return;
         }
 
