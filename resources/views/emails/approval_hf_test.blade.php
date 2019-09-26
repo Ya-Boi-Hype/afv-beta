d608a9315b553d459a85afa3d544a87c696698b3
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-09-26 02:14:37 +0200

    Back to list, change font size

--- resources/views/emails/approval_hf_test.blade.php
+++ resources/views/emails/approval_hf_test.blade.php
@@ -11,12 +11,8 @@ We are nearing the release of <i>Audio For VATSIM</i> and as such, would like to
 This test will take place on <u>Sunday, 29th September 2019</u>, with departures starting at 0800z. Our goal for this test is to find any potential bugs with HF transmissions.
 
 The event airports are shown below. You can fly in between any of them:
-
-|  DEP. Airports |   | ARR. Airports |
-|:--------------:|:-:|:-------------:|
-|      EFHK      |   |      CYQX     |
-|      ESSA      |   |      CYYZ     |
-|      ENGM      |   |      CYUL     |
+- Departures: EFHK - ESSA - ENGM
+- Arrivals: CYQX - CYYZ - CYUL
 
 For more information please login to the <i>Audio For VATSIM</i> website. Should you still have any other questions <b>after having read the documents available</b>, you may find our support channels there.
 
