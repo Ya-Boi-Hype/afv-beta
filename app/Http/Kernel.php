92f9f2146eefb4dd4b8c992b4970509ab29076b4
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2019-10-09 13:44:08 +0200

    Mini-bug fixes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
@@ -31,7 +32,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\CheckDomain::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
@@ -78,7 +79,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewarePriority = [
-        \Illuminate\Session\Middleware\StartSession::class,
+        // \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\Authenticate::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
--- app/Models/User.php
+++ app/Models/User.php
@@ -68,7 +68,10 @@ class User extends Authenticatable
         try {
             return Cache::rememberForever('permissions'.$this->id, function () {
                 $permissions = AfvApiController::getPermissions($this->id);
-                $permissions = array_diff($permissions, ['Facility Engineer']);
+                $permissions = array_diff($permissions, [
+                    'Facility Engineer',
+                    'User Permission Read',
+                ]);
 
                 return $permissions;
             });
