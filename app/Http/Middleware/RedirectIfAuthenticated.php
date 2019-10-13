910b0f0a03fee5d38c21bf337aa19d5b27d9e275
Aidan Stevens <aidan@aidanstevens.ca>
2019-10-13 19:35:03 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check() && ! count(auth()->user()->permissions)) {
-			return redirect()->intended(route('home'));
+            return redirect()->intended(route('home'));
         }
 
         return $next($request);
