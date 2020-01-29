d43e5120099f1df30abfdee68798a23bfc32ecf5
nepetadosmil <25409753+nepetadosmil@users.noreply.github.com>
2020-01-30 00:47:39 +0100

    \

--- app/Console/Commands/UpdateRatings.php
+++ app/Console/Commands/UpdateRatings.php
@@ -3,8 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use \Vatsim\Xml\Facades\Xml as VatsimXML;
 use App\Models\User;
-use Vatsim\Xml\Facades\Xml as VatsimXML;
 
 class UpdateRatings extends Command
 {
